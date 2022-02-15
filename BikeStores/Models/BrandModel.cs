45b9a94125f41df1a8fbcd4f4c91534b61956049
idgregory <idgregory12@gmail.com>
2022-02-14 20:26:42 -0600

    Added Products by Brand

---
+++ BikeStores/Models/BrandModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeStores.Models
+{
+    public class BrandModel
+    {
+        public string Brand { get; set; }
+        public string Product { get; set; }
+        public string Store { get; set; }
+        public int  Quantity { get; set; }
+    }
+}
